8b32a5217731ecfb88e8dc816973a72454424036
Ranen <fernandesranen05@gmail.com>
2025-02-22 18:18:11 +0000

    Player Movement
    Basic Player Movement added!

--- Everybody HAS a Birthday/Assets/Scripts/PlayerController.cs
+++ Everybody HAS a Birthday/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] private Rigidbody2D pRB;
+    [SerializeField] private float playerSpeed = 20;
+
+    private float _xInput;
+    private float _yInput;
+    
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
+    private void FixedUpdate()
+    {
+        pRB.velocity = new Vector2 (_xInput, _yInput) * playerSpeed * Time.deltaTime;
+    }
+
     // Update is called once per frame
     void Update()
     {
-        
+        _xInput = Input.GetAxis("Horizontal");
+        _yInput = Input.GetAxis("Vertical");
+
+        print(new Vector2(_xInput, _yInput));
     }
 }
