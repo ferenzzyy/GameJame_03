482632dda3ea5a2d6af42ef12c5152ebf5871756
Ranen <fernandesranen05@gmail.com>
2025-02-24 16:03:11 +0000

    Interactable basic functionality
    Dispenser Interaction done!

--- Everybody HAS a Birthday/Assets/Scripts/Interactable.cs
+++ Everybody HAS a Birthday/Assets/Scripts/Interactable.cs
@@ -4,8 +4,34 @@ using UnityEngine;
 
 public class Interactable : MonoBehaviour
 {
+    private bool _canInteract;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        if (collision.CompareTag("Interactable"))
+        {
+            _canInteract = true;
+
+        }
         
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Interactable"))
+        {
+            _canInteract = false;
+            
+        }
+    }
+
+    public bool GetCanInteract()
+    {
+        return _canInteract;
+    }
+
+    public void SetCanInteract(bool interact)
+    {
+        _canInteract = interact;
     }
 }
--- Everybody HAS a Birthday/Assets/Scripts/PlayerController.cs
+++ Everybody HAS a Birthday/Assets/Scripts/PlayerController.cs
@@ -7,49 +7,50 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private Rigidbody2D pRB;
     [SerializeField] private float playerSpeed = 20;
 
+    Interactable interact;
+
     private float _xInput;
     private float _yInput;
 
-    private bool _hasInteracted = false;
+    private void Awake()
+    {
+        interact = GetComponent<Interactable>();
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     private void FixedUpdate()
     {
         // Applying Player movement with input and speed
-        pRB.velocity = new Vector2 (_xInput, _yInput) * playerSpeed * Time.deltaTime;
+        pRB.velocity = new Vector2(_xInput, _yInput) * playerSpeed * Time.deltaTime;
     }
 
     // Update is called once per frame
     void Update()
     {
         InputHandler();
-     
+
 
     }
 
 
-    private void InputHandler() 
+    private void InputHandler()
     {
         // Player Input 
         _xInput = Input.GetAxis("Horizontal");
         _yInput = Input.GetAxis("Vertical");
 
-        if (Input.GetKeyDown(KeyCode.E))
+        if (Input.GetKeyDown(KeyCode.E) && interact.GetCanInteract())
         {
-            //print("Interact Button");
-            _hasInteracted = true;
+            print("Interacted!");
+
 
         }
 
     }
 
-    public bool GetHasInteracted()
-    {
-        return _hasInteracted;
-    }
 }
