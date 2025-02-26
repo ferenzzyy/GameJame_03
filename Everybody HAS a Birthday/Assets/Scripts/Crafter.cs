9ad9fe23660ab13a078e6bd3b8a8be68cdddd202
Ranen <fernandesranen05@gmail.com>
2025-02-26 15:39:22 +0000

    Crafting system basic functionality
    Very basic and crude crafting system so the player MUST put the components in order to complete the recipe

--- Everybody HAS a Birthday/Assets/Scripts/Crafter.cs
+++ Everybody HAS a Birthday/Assets/Scripts/Crafter.cs
@@ -1,8 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Crafter : MonoBehaviour
 {
-    private string[] components = {null};
+    private List<string> _components = new List<string>();
+    
+    [SerializeField] private List<GameObject> completedGifts = new List<GameObject>();
+
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -10,26 +15,70 @@ public class Crafter : MonoBehaviour
         {
             GiftComponent _giftComp = collision.GetComponent<GiftComponent>();
 
-            if (_giftComp != null) 
+            if (_giftComp != null && _components.Count < 3)
             {
-                components[0] = _giftComp.componentType.ToString();
-
-                
-
-                
+                _components.Add(_giftComp.componentType.ToString());
 
 
-                //print(_giftComp.componentType.ToString());
-                for (int i = 0; i < components.Length; i++)
+                Destroy(collision.gameObject);
+
+                string _resultRecipe = string.Join(", ", _components.ToArray());
+                CheckForRecipe(_resultRecipe);
+
+                for (int i = 0; i < _components.Count; i++)
                 {
-                    print(components[i]);
-                    
+                    print(_resultRecipe);
                 }
             }
+            else
+            {
+                Destroy(collision.gameObject);
+            } 
 
             
 
         }
+    }
+
+
+    private void CheckForRecipe(string _recipe)
+    {
+        switch (_recipe) 
+        {
+            case "METAL, METAL, METAL":
+                // SPAWN IN TUNGSTEN CUBE
+                print("CUBE!");
+                break;
+            case "METAL, METAL, PLASTIC":
+                print("MODEL CAR!");
+                break;
+            case "METAL, METAL, ELECTRONIC":
+                print("ROCKET");
+                break;
+            case "METAL, PLASTIC, ELECTRONIC":
+                print("LAPTOP/RC-CAR");
+                break;
+            case "PLASTIC, PLASTIC, ELECTRONIC":
+                print("PHONE");
+                break;
+            case "PLASTIC, ELECTRONIC, ELECTRONIC":
+                print("TRANSFORMER");
+                break;
+            case "PLASTIC, PLASTIC, PLASTIC":
+                print("VIYNL FIGURINE");
+                break;
+            case "PLASTIC, PLASTIC, METAL":
+                print("SUPERHERO FIGURINE");
+                break;
+            case "ELECTRONIC, ELECTRONIC, METAL":
+                print("POWER BANK");
+                break;
+            case "ELECTRONIC, ELECTRONIC, ELECTRONIC":
+                print("ZEUS");
+                break;
+
+        }
+
     }
 
 }
