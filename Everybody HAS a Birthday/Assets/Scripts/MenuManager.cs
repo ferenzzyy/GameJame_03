7c63373755d4e63655d954a3997f4a25ad9317f5
Ranen <fernandesranen05@gmail.com>
2025-02-28 12:03:09 +0000

    Basic Main Menu done
    Different menus for the main menu with buttons to start game go to options menu and quit the game.

---
+++ Everybody HAS a Birthday/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MenuManager : MonoBehaviour
+{
+    [SerializeField] GameObject[] menus;
+    // Start is called before the first frame update
+    void Start()
+    {
+        CheckForScene();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //CheckForScene();
+    }
+
+    void CheckForScene()
+    {
+        if (SceneManager.GetActiveScene().buildIndex == 0)
+        {
+            menus[0].SetActive(true);
+            menus[1].SetActive(false);
+            menus[2].SetActive(false);
+        }
+    }
+
+
+    public void StartButton()
+    {
+        print("Game Start");
+        menus[0].SetActive(false);
+        menus[1].SetActive(false);
+        menus[2].SetActive(false);
+    }
+
+    public void OptionsButton()
+    {
+        print("options");
+        menus[0].SetActive(false);
+        menus[1].SetActive(false);
+        menus[2].SetActive(true);
+    }
+
+    public void QuitButton()
+    {
+        Application.Quit();
+        print("Quit Game!");
+    }
+
+    public void BackButton()
+    {
+        menus[0].SetActive(true);
+        menus[1].SetActive(false);
+        menus[2].SetActive(false);
+    }
+}
